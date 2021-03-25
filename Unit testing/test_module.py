8e51705102a280ec6bfd7029b55254834220ed7e
PamelaMunozCaceres <pamelamunozcaceres@gmail.com>
2021-03-24 22:49:08 -0300

    ADD assert messages, assert floats problems, multiple asserts

--- Unit testing/test_module.py
+++ Unit testing/test_module.py
@@ -1,8 +1,41 @@
 import pytest
-from utils_module import function_1
+from utils_module import \
+    (function_1, compute_area_with_bug, compute_area)
+
 
 def test_first_function():
+    """
+    this test should be successful
+    """
     assert function_1() == 1
+
     
 def test_first_function_bad():
+    """
+    this test should fail
+    """
     assert function_1() == 0
+    
+    
+def test_compute_area_bug():
+    """
+    this test should fail if we pass a string it
+    won't transform it to float
+    """
+    actual_value = compute_area_with_bug(2, "3")
+    expected_value = 6
+    msg = (f"expected value is {expected_value},"
+           f" actual value is {actual_value}")
+    assert actual_value == expected_value, msg
+
+    
+def test_compute_area():
+    """
+    this test should be successful
+    """
+    actual_value = compute_area(2, "3")
+    expected_value = 6
+    msg = (f"expected value is {expected_value},"
+           f" actual value is {actual_value}")
+    assert isinstance(actual_value, float)
+    assert actual_value == expected_value, msg
--- Unit testing/utils_module.py
+++ Unit testing/utils_module.py
@@ -1,2 +1,16 @@
 def function_1():
     return 1
+
+
+def compute_area_with_bug(a, b) -> float:
+    """
+    calculate the area of a rectangle
+    """
+    return a * b
+
+
+def compute_area(a, b) -> float:
+    """
+    calculate the area of a rectangle
+    """
+    return float(a) * float(b)
